d267a4c372a932641a2fef063c03abdb05db183c
Alone0506 <wii03903975@gmail.com>
2022-04-28 14:19:10 +0800

    0428_1419

--- discord_bot.py
+++ discord_bot.py
@@ -4,7 +4,7 @@
 # ############
 
 import discord
-import logging
+# import logging
 from discord_components import DiscordComponents, ComponentsBot, Button
 from discord.ext import commands, tasks
 
@@ -17,14 +17,14 @@ DiscordComponents(bot)
 emoji = '\U0001F493'
 
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log',
-                              encoding='utf-8',
-                              mode='w')
-handler.setFormatter(logging.Formatter(
-    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+# logger = logging.getLogger('discord')
+# logger.setLevel(logging.DEBUG)
+# handler = logging.FileHandler(filename='discord.log',
+#                               encoding='utf-8',
+#                               mode='w')
+# handler.setFormatter(logging.Formatter(
+#     '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+# logger.addHandler(handler)
 
 
 @bot.event
